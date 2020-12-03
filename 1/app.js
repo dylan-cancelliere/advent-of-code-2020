c8a36bb6938f197c184c16c5c388b28480efcc76
Dylan Cancelliere <dycancel@gmail.com>
2020-12-03 18:12:28 -0500

    Part 2

--- 1/app.js
+++ 1/app.js
@@ -10,14 +10,34 @@ function main() {
         report.forEach(function (num) {
             dict[num] = 1;
         })
-        report.forEach(function (num) {
-            let temp = 2020 - num;
-            if (dict[temp]) {
-                console.log("Num: " + num + ", temp: " + temp);
-                console.log(temp * num);
-            }
-        });
+        console.log("Part 1: " + twoNums(dict, report) + "\n");
+        console.log("Part 2: " + threeNums(report) + "\n");
     })
+}
+
+function threeNums(report) {
+    for (let i = 0; i < report.length; i++) {
+        let num1 = parseInt(report[i]);
+        for (let j = i + 1; j < report.length; j++) {
+            let num2 = parseInt(report[j]);
+            if (num2 + num1 > 2020) continue;
+            for (let k = j + 1; k < report.length; k++) {
+                let num3 = parseInt(report[k]);
+                if (num1 + num2 + num3 == 2020) return num1 * num2 * num3;
+            }
+        }
+    }
+}
+
+function twoNums(dict, report) {
+    for (let i = 0; i < report.length; i++) {
+        let num = report[i];
+        let temp = 2020 - num;
+        if (dict[temp]) {
+            return temp * num;
+        }
+    }
+
 }
 
 main();
