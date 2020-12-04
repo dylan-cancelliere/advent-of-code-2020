5a927efa59b36b6818b3c7f2eac05a5f51a35601
Dylan Cancelliere <dycancel@gmail.com>
2020-12-03 19:32:42 -0500

    Day 2 part 2

--- 2/app.js
+++ 2/app.js
@@ -9,7 +9,25 @@ function main(){
 
         let arr = data.toString().split("\n");
         console.log("Part 1: " + numValidPasswords(arr) + "\n");
+        console.log("Part 2: " + updatedPasswords(arr) + "\n");
     });
+}
+
+function updatedPasswords(data){
+    let counter = 0;
+    data.forEach(function(str){
+        str = str.split(" ");
+        
+        let positions = str[0].split("-");
+        let pos1 = positions[0] - 1;
+        let pos2 = positions[1] - 1;
+        let letter = str[1].substring(0, 1);
+
+        let result1 = str[2].substring(pos1, pos1 + 1) == letter;
+        let result2 = str[2].substring(pos2, pos2 + 1) == letter;
+        if (!(result1 && result2) && (result1 || result2)) counter++;
+    })
+    return counter;
 }
 
 function numValidPasswords(data){
