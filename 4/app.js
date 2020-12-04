d2bbee6fe7ac39ba788ca36cea1a0ee9dcf99b4a
Dylan Cancelliere <dycancel@gmail.com>
2020-12-04 02:02:56 -0500

    Day 4 part 2

--- 4/app.js
+++ 4/app.js
@@ -19,7 +19,61 @@ function checkPassport(data){
             if (data[i] == '') break;
 
             data[i].split(" ").forEach(function(pair){
-                set.add(pair.split(":")[0]);
+                let field = pair.split(":")[0];
+                let val = pair.split(":")[1];
+                let pattern, num, temp;
+                switch (field){
+                    case "byr":
+                        if (isNaN(val)) break;
+                        val = parseInt(val);
+                        if (val >= 1920 && val <= 2002) set.add(field);
+                        break;
+                    case "iyr":
+                        if (isNaN(val)) break;
+                        val = parseInt(val);
+                        if (val >= 2010 && val <= 2020) set.add(field);
+                        break;
+                    case "eyr":
+                        if (isNaN(val)) break;
+                        val = parseInt(val);
+                        if (val >= 2020 && val <= 2030) set.add(field);
+                        break;
+                    case "hgt":
+                        pattern = /[0-9]+(cm|in)/g;
+                        temp = pattern.exec(val)
+                        if (temp != null && temp[0] == val){
+                            if (val.substring(val.length - 2, val.length) == "cm"){
+                                num = parseInt(val.substring(0, val.length - 2));
+                                if (num >= 150 && num <= 193) {
+                                    set.add(field);
+                                    break;
+                                }
+                            }else{
+                                num = parseInt(val.substring(0, val.length - 2));
+                                if (num >= 59 && num <= 76) {
+                                    set.add(field);
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    case "hcl":
+                        pattern = /#[0-9a-f]{6}/g;
+                        temp = pattern.exec(val)
+                        if (temp != null && temp[0] == val) set.add(field);
+                        break;
+                    case "ecl":
+                        if (val == 'amb' || val == 'blu' || val == 'brn' || val == 'gry' || val == 'grn' || val == 'hzl' || val == 'oth'){
+                            set.add(field);
+                        }
+                        break;
+                    case "pid":
+                        if (val.length == 9 && !isNaN(val)) set.add(field);
+                        break;
+                    case "cid":
+                        set.add(field);
+                        break;
+                }
             });
             i++;
         }
