2a40ebd20603c280db957e23d950914e22f3cb88
Dylan Cancelliere <dycancel@gmail.com>
2020-12-23 12:34:44 -0500

    Day 11 part 2

--- 11/app.js
+++ 11/app.js
@@ -1,7 +1,7 @@
 main()
 
 function main() {
-    const fname = "/home/dycancel/advent-of-code-2020/11/input.txt";
+    const fname = "/home/spyro/advent-of-code-2020/11/input.txt";
     const fs = require('fs');
 
     fs.readFile(fname, (err, data) => {
@@ -21,45 +21,37 @@ function part1(data){
         let dataCopy = JSON.parse(JSON.stringify(data));
         for (let i = 0; i < data.length; i++){
             for (let j = 0; j < data[i].length; j++){
-                let full = 0, empty = 0;
+                let full = 0;
                 // top left
                 if (data[i-1] && data[i-1][j-1]){
-                    if (data[i-1][j-1] == 'L') empty++;
                     if (data[i-1][j-1] == '#') full++;
                 }
                 // top middle
                 if (data[i-1] && data[i-1][j]){
-                    if (data[i-1][j] == 'L') empty++;
                     if (data[i-1][j] == '#') full++;
                 }
                 // top right
                 if (data[i-1] && data[i-1][j+1]){
-                    if (data[i-1][j+1] == 'L') empty++;
                     if (data[i-1][j+1] == '#') full++;
                 }
                 // middle right
                 if (data[i] && data[i][j+1]){
-                    if (data[i][j+1] == 'L') empty++;
                     if (data[i][j+1] == '#') full++;
                 }
                 // bottom right
                 if (data[i+1] && data[i+1][j+1]){
-                    if (data[i+1][j+1] == 'L') empty++;
                     if (data[i+1][j+1] == '#') full++;
                 }
                 // bottom middle
                 if (data[i+1] && data[i+1][j]){
-                    if (data[i+1][j] == 'L') empty++;
                     if (data[i+1][j] == '#') full++;
                 }
                 // bottom left
                 if (data[i+1] && data[i+1][j-1]){
-                    if (data[i+1][j-1] == 'L') empty++;
                     if (data[i+1][j-1] == '#') full++;
                 }
                 // middle left
                 if (data[i] && data[i][j-1]){
-                    if (data[i][j-1] == 'L') empty++;
                     if (data[i][j-1] == '#') full++;
                 }
                 if (data[i] && data[i][j] == 'L' && full === 0){
@@ -84,5 +76,97 @@ function part1(data){
 }
 
 function part2(data){
-
+    for (let i = 0; i < data.length; i++){
+        data[i] = data[i].split('');
+    }
+    let bool = true;
+    while (bool){
+        bool = false;
+        let dataCopy = JSON.parse(JSON.stringify(data));
+        for (let i = 0; i < data.length; i++){
+            for (let j = 0; j < data[i].length; j++){
+                let full = 0;
+                // top left
+                for (let k = i, l = j; data[k-1] && data[k-1][l-1]; k--, l--){
+                    if (data[k-1][l-1] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k-1][l-1] == 'L') break;
+                }
+                // top middle
+                for (let k = i, l = j; data[k-1] && data[k-1][l]; k--){
+                    if (data[k-1][l] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k-1][l] == 'L') break;
+                }
+                // top right
+                for (let k = i, l = j; data[k-1] && data[k-1][l+1]; k--, l++){
+                    if (data[k-1][l+1] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k-1][l+1] == 'L') break;
+                }                
+                // middle right
+                for (let k = i, l = j; data[k] && data[k][l+1]; k, l++){
+                    if (data[k][l+1] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k][l+1] == 'L') break;
+                }
+                // bottom right
+                for (let k = i, l = j; data[k+1] && data[k+1][l+1]; k++, l++){
+                    if (data[k+1][l+1] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k+1][l+1] == 'L') break;
+                }
+                // bottom middle
+                for (let k = i, l = j; data[k+1] && data[k+1][l]; k++){
+                    if (data[k+1][l] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k+1][l] == 'L') break;
+                }
+                // bottom left
+                for (let k = i, l = j; data[k+1] && data[k+1][l-1]; k++, l--){
+                    if (data[k+1][l-1] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k+1][l-1] == 'L') break;
+                }
+                // middle left
+                for (let k = i, l = j; data[k] && data[k][l-1]; l--){
+                    if (data[k][l-1] == '#'){
+                        full++;
+                        break;
+                    }
+                    if (data[k][l-1] == 'L') break;
+                }
+                if (data[i] && data[i][j] == 'L' && full === 0){
+                    dataCopy[i][j] = '#';
+                    bool = true;
+                }
+                if (data[i] && data[i][j] == '#' && full >= 5){
+                    bool = true;
+                    dataCopy[i][j] = 'L';
+                }
+            }
+        }
+        data = JSON.parse(JSON.stringify(dataCopy));
+    }
+    let counter = 0;
+    for (let i = 0; i < data.length; i++){
+        for (let j = 0; j < data[i].length; j++){
+            if (data[i][j] == '#') counter++;
+        }
+    }
+    return counter;
 }
